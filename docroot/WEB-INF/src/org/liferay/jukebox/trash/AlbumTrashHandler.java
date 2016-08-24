1466aa3977b2457e9a0c1c6c7fcf1484f3276807
Eduardo Garcia <eduardo.garcia@liferay.com>
2016-08-24 17:58:24 +0200

    MIGRATION: Update TrashHandler Implementation

--- docroot/WEB-INF/src/org/liferay/jukebox/trash/AlbumTrashHandler.java
+++ docroot/WEB-INF/src/org/liferay/jukebox/trash/AlbumTrashHandler.java
@@ -19,6 +19,7 @@ import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.trash.TrashRenderer;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.trash.kernel.model.TrashEntry;
 
